16a15870a256216692d3e06a8bcf26e785eea972
Prateek Chandra <Prateek.Chandra@datacore.com>
2022-12-08 15:57:20 +0530

    fix: added helm scripts and github actions
    Signed-off-by: Prateek Chandra <Prateek.Chandra@datacore.com>

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{}:
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {
+    overlays = [ (_: _: { inherit sources; }) ];
+  };
+in
+with pkgs;
+let
+in
+mkShell {
+  name = "helm-scripts-shell";
+  buildInputs = [
+    git
+    semver-tool
+    yq-go
+  ];
+}
