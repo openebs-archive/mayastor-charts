e04aff6901bffdfa906960227db7cba816486757
Tiago Castro <tiagolobocastro@gmail.com>
2023-01-11 17:25:34 +0000

    chore(shell): add commitlinter as git pre-commit hook
    Add the commitlinter as a pre-commit hook
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -2,17 +2,29 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
-    overlays = [ (_: _: { inherit sources; }) ];
+    overlays = [ (_: _: { inherit sources; }) (import ./nix/overlay.nix { }) ];
   };
 in
 with pkgs;
 let
 in
 mkShell {
-  name = "helm-scripts-shell";
+  name = "helm-chart-shell";
+
+  NODE_PATH = "${nodePackages."@commitlint/config-conventional"}/lib/node_modules";
+
   buildInputs = [
+    commitlint
     git
+    kubernetes-helm-wrapped
+    niv
+    pre-commit
     semver-tool
     yq-go
   ];
+
+  shellHook = ''
+    pre-commit install
+    pre-commit install --hook commit-msg
+  '';
 }
